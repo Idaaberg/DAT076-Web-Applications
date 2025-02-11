1f6a11e5f34c775d5963d26e3289d1ab0c10b43e
Ida Åberg <idaaberg0310@gmail.com>
2025-02-11 11:04:45 +0100

    Lab 3 setup
    Accidentally set up lab 3 in the lab 2 branch
    
    Co-Authored-By: Erik Dreifeldt <94061173+erikdreifeldt1@users.noreply.github.com>
    Co-Authored-By: maddlar <93659425+maddlar@users.noreply.github.com>

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { bookRouter } from "./router/book";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/book", bookRouter);
