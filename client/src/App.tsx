a37d7764ddac066f4cc05171825c553d3b73c5b7
Ida Åberg <idaaberg0310@gmail.com>
2025-03-01 16:48:26 +0100

    Move BrowserRouter from App.tsx to Main.tsx
    To be able to use MemoryRoutes in testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import AddBook from './pages/AddBook';
 import EditBook from './pages/EditBook';
@@ -9,17 +9,14 @@ import Register from './pages/Register';
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/add" element={<AddBook />} />
-        <Route path="/edit/:id" element={<EditBook />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/add" element={<AddBook />} />
+      <Route path="/edit/:id" element={<EditBook />} />
+    </Routes>
   );
 }
 export default App
