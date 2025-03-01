3af55b15fd77d6f8552c919386bfa29485205753
Ida Åberg <idaaberg0310@gmail.com>
2025-03-01 17:14:45 +0100

    Create Login.test.tsx

---
+++ client/src/tests/Login.test.tsx
@@ -0,0 +1,35 @@
+import { render, fireEvent, act } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import axios from 'axios';
+import { MemoryRouter } from 'react-router';
+// import Register from '../pages/Register';
+import Login from '../pages/Login';
+
+jest.mock("axios")
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('A Login screen', () => {
+  test('when users have registered, they can log in', async () => {
+    mockedAxios.post.mockResolvedValueOnce({ data: { token: "fake-jwt-token" } });
+
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+
+    const loginUsernameField = await screen.findByLabelText("Username");
+    const loginPasswordField = await screen.findByLabelText("Password");
+    const loginButton = screen.getByRole('button', { name: "Log In" });
+
+    await act(async () => {
+      fireEvent.change(loginUsernameField, { target: { value: "JohnDoe" } });
+      fireEvent.change(loginPasswordField, { target: { value: "drowssap" } });
+      fireEvent.click(loginButton);
+    });
+
+    expect(mockedAxios.post).toHaveBeenCalledWith('http://localhost:8080/user/login',
+      { username: 'JohnDoe', password: 'drowssap' }
+    );
+  });
+});
