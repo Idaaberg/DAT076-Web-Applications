93c068aa3152ac59898d16911103c8cfc16b0580
Ida Åberg <idaaberg0310@gmail.com>
2025-03-06 10:25:45 +0100

    Add interfaces for User and Book
    Co-Authored-By: ciccinne <117648894+ciccinne@users.noreply.github.com>
    Co-Authored-By: Erik Dreifeldt <94061173+erikdreifeldt1@users.noreply.github.com>
    Co-Authored-By: maddlar <93659425+maddlar@users.noreply.github.com>

---
+++ server/src/service/IBookService.interface.ts
@@ -0,0 +1,8 @@
+import { Book } from "../model/book.interface";
+
+export interface IBookService {
+    getBooks(username: string): Promise<Book[] | undefined>;
+    getBookById(username: string, id: number): Promise<Book | undefined>;
+    addBook(username: string, title: string, author: string, state: string, rating?: number, comment?: string): Promise<Book | undefined>;
+    editBook(username: string, id: number, title: string, author: string, state: string, rating?: number, comment?: string): Promise<Book | undefined>;
+}
