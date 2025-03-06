d48094bd0d0066062080aae0b4312e27adcf9a49
Ida Åberg <idaaberg0310@gmail.com>
2025-03-06 10:26:10 +0100

    Create user.db.ts
    Co-Authored-By: ciccinne <117648894+ciccinne@users.noreply.github.com>
    Co-Authored-By: Erik Dreifeldt <94061173+erikdreifeldt1@users.noreply.github.com>
    Co-Authored-By: maddlar <93659425+maddlar@users.noreply.github.com>

---
+++ server/src/db/user.db.ts
@@ -0,0 +1,48 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes } from "sequelize";
+import { Association } from "sequelize";
+import { conn } from "./conn";
+import { BookModel } from "./book.db";
+
+
+export class UserModel extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare password: string;
+  declare static associations: {
+    books: Association<UserModel, BookModel>;
+  };
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    }
+  },
+  {
+    sequelize: conn,
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+UserModel.hasMany(BookModel, {
+  sourceKey: 'id',
+  foreignKey: 'userId',
+  as: 'tasks'
+});
