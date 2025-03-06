4e47c3bfd84f1659dca2eba01d244debc2f49d43
Ida Åberg <idaaberg0310@gmail.com>
2025-03-06 10:28:37 +0100

    Update user service for using db
    Co-Authored-By: ciccinne <117648894+ciccinne@users.noreply.github.com>
    Co-Authored-By: Erik Dreifeldt <94061173+erikdreifeldt1@users.noreply.github.com>
    Co-Authored-By: maddlar <93659425+maddlar@users.noreply.github.com>

--- server/src/service/user.ts
+++ server/src/service/user.ts
@@ -1,28 +1,33 @@
 import { User } from "../model/user";
 import bcrypt from "bcrypt";
+import { IUserService } from "./IUserService.interface";
+import { UserModel } from "../db/user.db";
 
 const salt = bcrypt.genSaltSync(10);
 
-export class UserService {
-    users: User[] = [];
+export class UserService implements IUserService {
 
-    async createUser(username: string, password: string) {
-        this.users.push({
+    async createUser(username: string, password: string) : Promise<UserModel | null> {
+        if (await UserModel.findOne({ where: { username: username }})) {
+            return null;
+        }
+        return await UserModel.create({
             username: username,
-            password: bcrypt.hashSync(password, salt),
-            books: []
+            password: bcrypt.hashSync(password, salt)
         });
     }
 
     async findUser(username: string, password?: string): Promise<User | undefined> {
-        const user = this.users.find((user) => user.username === username);
-        if (!user) return undefined;
-
-        if (password) {
+        const user = await UserModel.findOne({ where: { username: username }});
+        if (! password) {
+            const foundUser = await UserModel.findOne({ where: { username }});
+            return foundUser ? foundUser : undefined;
+        }   
+        if (password && user) {
             const match = await bcrypt.compare(password, user.password); 
             if (!match) return undefined;
+            const foundUser = await UserModel.findOne({ where: { username: username, password: user.password }});
+            return foundUser ? foundUser : undefined;
         }
-
-        return user;
     }
 }
