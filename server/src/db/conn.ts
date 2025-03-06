4c67385cbc87906e1905bbf870efb6da661472c1
Ida Åberg <idaaberg0310@gmail.com>
2025-03-06 15:23:33 +0100

    Add test for BookService and UserService

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -5,7 +5,15 @@ export let conn : Sequelize;
 
 dotenv.config();
 
-conn = new Sequelize(`postgres://app_db_user:${process.env.DB_PASSWORD}@localhost:54321`);
+if (process.env.NODE_ENV === "test") {
+  conn = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',  
+  });
+  console.log("Using in-memory database");
+} else {
+  conn = new Sequelize(`postgres://app_db_user:${process.env.DB_PASSWORD}@localhost:54321`);
+}
 
 export async function initDB() {
     await conn.sync({alter: true, force: false});
