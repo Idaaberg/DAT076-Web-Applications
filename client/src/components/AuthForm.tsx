40eaf9c1246b68243d3f1a25ab003e8ef2cbe5eb
Ida Åberg <idaaberg0310@gmail.com>
2025-03-10 15:30:26 +0100

    Add frontend validation for login and register
    Makes sure the password is atleast 8 chars and username is unique

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "../styles/login.css";
+import { Errors } from "../types/types";
+
 
 interface AuthFormProps {
     title: string;
@@ -7,9 +9,10 @@ interface AuthFormProps {
     onSubmit: (username: string, password: string) => Promise<void>;
     linkText: string;
     linkTo: string;
+    errors:  Errors;
 }
 
-function AuthForm({ title, buttonText, onSubmit, linkText, linkTo }: AuthFormProps) {
+function AuthForm({ title, buttonText, onSubmit, linkText, linkTo, errors }: AuthFormProps) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -33,23 +36,23 @@ function AuthForm({ title, buttonText, onSubmit, linkText, linkTo }: AuthFormPro
                     <label htmlFor="username">Username</label>
                     <input
                         id="username"
-                        className="loginInput"
+                        className={`loginInput ${errors.username ? 'inputError' : ''}`}
                         type="text"
                         placeholder="Username"
                         value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
+                        onChange={(e) => setUsername(e.target.value)}   
                     />
+                    {errors.username && <p className="error">{errors.username}</p>}
                     <label htmlFor="password">Password</label>
                     <input
                         id="password"
-                        className="loginInput"
+                        className={`loginInput ${errors.password ? 'inputError' : ''}`}
                         type="password"
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        required
                     />
+                    {errors.password && <p className="error">{errors.password}</p>}
                     <div className="formBtns">
                         <button className="submitBtn" type="submit">{buttonText}</button>
                         <a href={linkTo}>
--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response, Router } from "express";
+import express, { Request, Response, Router, RequestHandler } from "express";
 import { User } from "../model/user";
 import { IUserService } from "../service/IUserService.interface";
 
+interface UserRequest extends Request {
+    body: { username: string, password: string },
+    session: any
+}
+
+interface UsernameQuery {
+    username: string;
+  }
+
 export function userRouter(userService: IUserService): Router {
     const userRouter = express.Router();
-
-    interface UserRequest extends Request {
-        body: { username: string, password: string },
-        session: any
-    }
 
     userRouter.post("/", async (req: UserRequest, res: Response) => {
         await userService.createUser(req.body.username, req.body.password);
@@ -29,6 +33,22 @@ export function userRouter(userService: IUserService): Router {
         delete req.session.username;
         res.status(200).send("Logged out");
     })
+
+    const existsHandler: RequestHandler<{}, any, any, UsernameQuery> = async (req, res) => {
+        const { username } = req.query;
+        if (!username) {
+          res.status(400).send({ error: "Username query parameter is required" });
+          return;
+        }
+        try {
+          const user = await userService.findUser(username);
+          res.status(200).json({ exists: !!user });
+        } catch (error) {
+          res.status(500).send("Internal server error");
+        }
+      };
+    
+    userRouter.get("/exists", existsHandler);
 
     return userRouter;
 }
