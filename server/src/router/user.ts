509fd4c752086e2fbdc82315f77a346754f596a5
Ida Åberg <idaaberg0310@gmail.com>
2025-03-13 14:09:30 +0100

    Update user router to handle internal server errors

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -16,25 +16,44 @@ export function userRouter(userService: IUserService): Router {
 
   // POST /user
   userRouter.post("/", async (req: UserRequest, res: Response) => {
-    await userService.createUser(req.body.username, req.body.password);
-    res.status(201).send({ username: req.body.username });
+    const { username, password } = req.body;
+    const existingUser = await userService.findUser(username);
+
+    try {
+      if (existingUser) {
+        res.status(400).send({ error: "Username already taken" });
+        return;
+      }
+      await userService.createUser(username, password);
+      res.status(201).send({ username: username });
+    } catch (error) {
+      res.status(500).send("Internal server error");
+    }
   })
 
   // POST /user/login
   userRouter.post("/login", async (req: UserRequest, res: Response) => {
     const user: User | undefined = await userService.findUser(req.body.username, req.body.password);
-    if (!user) {
-      res.status(401).send("No such username or password");
-      return;
+    try {
+      if (!user) {
+        res.status(401).send("No such username or password");
+        return;
+      }
+      req.session.username = req.body.username;
+      res.status(200).send("Logged in");
+    } catch (error) {
+      res.status(500).send("Internal server error");
     }
-    req.session.username = req.body.username;
-    res.status(200).send("Logged in");
   })
 
   // POST/user/logout
   userRouter.post("/logout", async (req: UserRequest, res: Response) => {
-    delete req.session.username;
-    res.status(200).send("Logged out");
+    try {
+      delete req.session.username;
+      res.status(200).send("Logged out");
+    } catch (error) {
+      res.status(500).send("Internal server error");
+    }
   })
 
   const existsHandler: RequestHandler<{}, any, any, UsernameQuery> = async (req, res) => {
@@ -56,11 +75,15 @@ export function userRouter(userService: IUserService): Router {
 
   //GET /user
   userRouter.get("/", async (req: UserRequest, res: Response) => {
-    if (!req.session.username) {
-      res.status(401).send("Not logged in");
-      return;
+    try {
+      if (!req.session.username) {
+        res.status(401).send("Not logged in");
+        return;
+      }
+      res.status(200).send({ username: req.session.username });
+    } catch (error) {
+      res.status(500).send("Internal server error");
     }
-    res.status(200).send({ username: req.session.username });
   });
 
   return userRouter;
